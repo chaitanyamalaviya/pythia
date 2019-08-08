623e8c95f6755f73372f9a7cfb7631fb632b85fe
chaitanya <chaitanyamalaviya@gmail.com>
2019-08-08 14:25:52 -0700

    change __init__ models

--- pythia/models/__init__.py
+++ pythia/models/__init__.py
@@ -1,7 +1,8 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
-__all__ = ["TopDownBottomUp", "Pythia", "LoRRA", "BAN"]
+__all__ = ["TopDownBottomUp", "Pythia", "LoRRA", "BAN", "BAN_with_Concepts"]
 
 from .top_down_bottom_up import TopDownBottomUp
 from .ban import BAN
 from .pythia import Pythia
 from .lorra import LoRRA
+from .ban_with_concepts import BAN_with_Concepts

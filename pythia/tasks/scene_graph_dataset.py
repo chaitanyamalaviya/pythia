49797450886554b8f33abc4785d982be6870524a
chaitanya <chaitanyamalaviya@gmail.com>
2019-08-07 18:06:39 -0700

    ban_with_concepts

---
+++ pythia/tasks/scene_graph_dataset.py
@@ -0,0 +1,45 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+import numpy as np
+import torch
+
+import json
+
+class SceneGraphDatabase(torch.utils.data.Dataset):
+    """
+    Dataset for SceneGraphs used in Pythia
+    """
+
+    def __init__(self, scene_graph_path):
+        super().__init__()
+
+        if not scene_graph_path.endswith(".json"):
+            raise ValueError("Unknown file format for scene graph file")
+
+        with open(scene_graph_path) as f:
+            data = f.readlines()
+            self.scene_graphs_db = json.loads(data[0])
+
+        self.data = self.process_scene_graphs()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, image_id):
+        return self.data[image_id]
+
+    def process_scene_graphs(self):
+        image_assertions = {}
+        for img_id in self.scene_graphs_db.keys():
+            image_assertions[img_id] = []
+            image_objects = {}
+            for obj_id in self.scene_graphs_db[img_id]["objects"].keys():
+                image_objects[obj_id] = self.scene_graphs_db[img_id]["objects"][obj_id]["name"]
+            for obj_id in self.scene_graphs_db[img_id]["objects"].keys():
+                obj_name = self.scene_graphs_db[img_id]["objects"][obj_id]
+                for rel in self.scene_graphs_db[img_id]["objects"][obj_id]["relations"]:
+                    rel_name = rel["name"]
+                    target_obj = image_objects[rel["object"]]
+                    image_assertions[img_id].append(obj_name + rel_name + target_obj)
+
+        return image_assertions
+
